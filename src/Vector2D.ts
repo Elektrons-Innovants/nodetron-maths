4c20218bbd0235f189ee7547e700626b475cad46
Etienne Schmitz <hello@etienne-schmitz.com>
2021-02-06 16:44:16 +0100

    feat: add norm and normalized (+ test)
    Add norm in vector and a way to normalize vector

--- src/Vector2D.ts
+++ src/Vector2D.ts
@@ -1,8 +1,21 @@
+/* eslint-disable class-methods-use-this */
 export interface AbstractVector {
   x: number,
   y: number,
 }
 
 export class Vector implements AbstractVector {
-  constructor(public x: number, public y: number) {}
+  constructor(public x: number, public y: number) { }
+
+  public norm(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y)
+  }
+
+  public normalized(): void {
+    const norm = this.norm()
+    if (norm !== 0) {
+      this.x /= norm
+      this.y /= norm
+    }
+  }
 }
