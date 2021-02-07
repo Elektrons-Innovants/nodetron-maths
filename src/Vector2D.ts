ff2a02be522e7764fe35a6a839f4a5970233b4f0
Etienne Schmitz <hello@etienne-schmitz.com>
2021-02-07 08:00:22 +0100

    docs: add docs

--- src/Vector2D.ts
+++ src/Vector2D.ts
@@ -1,27 +1,27 @@
 export interface AbstractVector {
   /**
-   * x-vector
+   * x-coordinate in 2D plan.
    */
   x: number,
   /**
-   * y-vector
+   * y-coordinate in 2D plan.
    */
   y: number,
 }
 
 export class Vector implements AbstractVector {
   /**
-   * Vector 2D constructor
+   * Vector 2D constructor.
    *
-   * @param x - The x of the vector
-   * @param y - The y of the vector
+   * @param x - The x of the vector.
+   * @param y - The y of the vector.
    */
   constructor(public x: number, public y: number) { }
 
   /**
    * Get the norm of the vector.
    *
-   * @return the norm of the vector
+   * @return the norm of the vector.
    */
   public norm(): number {
     return Math.sqrt(this.x * this.x + this.y * this.y)
